763b13925bbf1f945a0e396f9be2be26469ae253
FedericoGirlanda <fedegirla.fg@gmail.com>
2023-06-02 22:02:06 +0200

    Added references, features extraction and image thresholding

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
 #include "foodRecognition.h"
 #include "foodSegmentation.h"
@@ -6,6 +7,10 @@
 
 using namespace cv;
 using namespace std;
+
+#define NEIGHBORHOOD_X 9
+#define NEIGHBORHOOD_Y 9
+#define THRESHOLD Scalar(50,20,20)
 
 //bilateral filter because its smoothing when preserve the edges
 Mat applyBilateralFilter(const Mat &inputImage) {
@@ -68,18 +73,136 @@ Mat applyColorAdjustment(const Mat &inputImage) {
     return adjustedImage;
 }
 
-int main(int argc, char **argv) {
-    Mat inputImage = imread("/Users/hilalurun/CLionProjects/Computer_Vision_Project/dataset/tray1/food_image.jpg");
-    Mat filteredImage = applyBilateralFilter(inputImage);
-    Mat enhancedImage = applyContrastEnhancement(filteredImage);
-    Mat colorAdjustedImage = applyColorAdjustment(enhancedImage);
+void featMatching(Mat img1, Mat img2, Mat &Result_MATCHING){
+	double ratio = 1.5; //3;
+  cv::Ptr<cv::Feature2D> sift = cv::SIFT::create();
+  cv::Ptr<cv::FastFeatureDetector> fast = cv::FastFeatureDetector::create();
+  cv::FlannBasedMatcher Matcher_FLANN;
+  std::vector<cv::KeyPoint> keypoints1, keypoints2;
+  vector<cv::DMatch> matches, good_matches;
+  cv::Ptr<cv::DescriptorMatcher> Matcher_SIFT = cv::BFMatcher::create(cv::NORM_L2);			// Brute-Force matcher create method
+  Mat img1_gray, img2_gray, descr1, descr2;
 
-    // Display the original and processed images
-    imshow("Original Image", inputImage);
-    imshow("Filtered Image", filteredImage);
-    imshow("Enhanced Image", enhancedImage);
-    imshow("Color Adjusted Image", colorAdjustedImage);
+  /*sift->detectAndCompute(proj1, cv::Mat(), keypoints1, descr1);
+  sift->detectAndCompute(proj2, cv::Mat(), keypoints2, descr2);
+  Matcher_SIFT->match(descr1, descr2, matches);*/
+  fast->detect(img1, keypoints1);
+	fast->detect(img2, keypoints2);
+	sift->compute(img1, keypoints1, descr1);
+	sift->compute(img2, keypoints2, descr2);
+  Matcher_FLANN.match(descr1, descr2, matches);
+
+  // keeping only good matches (dist less than ratio*min_dist)
+  double max_dist = 0.0, min_dist = 200.0;
+  int min_idx;
+	for (int j = 0; j < matches.size(); j++)
+	{
+		double dist = matches[j].distance;
+		if (dist < min_dist)
+			min_dist = dist;
+      min_idx = j;
+		if (dist > max_dist)
+			max_dist = dist;
+	}
+
+
+
+  std::vector<cv::Point2f> good_keypoints1, good_keypoints2;
+	for (int j = 0; j < matches.size(); j++)
+	{
+		if (matches[j].distance <= ratio * min_dist)
+		{
+			good_matches.push_back(matches[j]);
+      good_keypoints1.push_back(keypoints1[matches[j].queryIdx].pt);
+      good_keypoints2.push_back(keypoints2[matches[j].trainIdx].pt);
+		}
+	}
+  cv::drawKeypoints(img1, keypoints1, Result_MATCHING);
+	//cv::drawMatches(img1, keypoints1, img2, keypoints2, good_matches, Result_MATCHING, cv::Scalar::all(-1), cv::Scalar(-1), vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+}
+
+void onMouse(int event, int x, int y, int f, void* userdata){
+  if(event == EVENT_LBUTTONDOWN){
+    // Retriving the image from main
+    Mat img = *(Mat*) userdata;
+    Mat img_hsv;
+    cvtColor(img, img_hsv, COLOR_BGR2HSV);
+
+    // Preventing segfaults for looking over the image boundaries
+    if (y + NEIGHBORHOOD_Y > img.rows || x + NEIGHBORHOOD_X > img.cols){
+      return;
+    }
+
+    // Getting the mean of the RGB colors of a 9x9 NEIGHBORHOOD around the selected pixel
+    Rect rect(x,y,NEIGHBORHOOD_X,NEIGHBORHOOD_Y);
+    Scalar mean_value = mean(img_hsv(rect));
+    std::cout << "mean = " << mean_value << std::endl;
+    //std::cout << "NEIGHBORHOOD = " << img_out(rect).at<Vec3b> (0,0) << std::endl;
+
+    // Mask computation
+    Mat mask0, mask1, mask2;
+    inRange(img_hsv, Scalar(mean_value[0]-THRESHOLD[0], mean_value[1], mean_value[2]), Scalar(mean_value[0]+THRESHOLD[0], 255, 255), mask0);
+    inRange(img_hsv, Scalar(mean_value[0], mean_value[1]-THRESHOLD[1], mean_value[2]), Scalar(255, mean_value[1]+THRESHOLD[1], 255), mask1);
+    inRange(img_hsv, Scalar(mean_value[0], mean_value[1], mean_value[2]-THRESHOLD[2]), Scalar(255, 255, mean_value[2]+THRESHOLD[2]), mask2);
+    Mat mask = mask0 | mask1 | mask2;
+
+    // Create and show the window
+  	namedWindow("Thresholding mask");
+  	imshow("Thresholding mask", mask);
     waitKey(0);
+    destroyWindow("Thresholding mask");
+    return;
+    }
+}
 
-    return 0;
+int main(int argc, char **argv) {
+  // Check if the image file is provided
+  if(argc < 2){
+    printf("Warning: image file not provided\n");
+    return -1;
+    }
+
+  // Read the images
+  Mat img1 = imread(argv[1]);
+  Mat img2 = imread(argv[2]);
+
+  // Check if the image file has been introduced corretly
+  if(img1.empty() || img2.empty()){
+    printf("Warning: image file not provided correctly. The name or the file type could be wrong.\n");
+    return -1;
+    }
+
+  // 0. IMAGE PREPROCESSING
+  Mat filteredImage1 = applyBilateralFilter(img1);
+  Mat enhancedImage1 = applyContrastEnhancement(filteredImage1);
+  Mat colorAdjustedImage1 = applyColorAdjustment(enhancedImage1);
+  Mat filteredImage2 = applyBilateralFilter(img2);
+  Mat enhancedImage2 = applyContrastEnhancement(filteredImage2);
+  Mat colorAdjustedImage2 = applyColorAdjustment(enhancedImage2);
+  Mat preprocImage11;
+  hconcat(img1, filteredImage1, preprocImage11);
+  Mat preprocImage12;
+  hconcat(enhancedImage1, colorAdjustedImage1, preprocImage12);
+  Mat preprocImage;
+  vconcat(preprocImage11, preprocImage12, preprocImage);
+  namedWindow("Image preprocessing", WINDOW_NORMAL);
+  imshow("Image preprocessing", preprocImage);
+  resizeWindow("Image preprocessing", Size(600,600));
+  waitKey(0);
+  //return 0;
+
+  // 1. IMAGE THRESHOLDING
+  namedWindow("Image thresholding");
+  imshow("Image thresholding", img1);
+  setMouseCallback("Image thresholding", onMouse, (void*)&img1);
+  waitKey(0);
+  //return 0;
+
+  // 2. FEATURE EXTRACTION
+  Mat Result_MATCHING;
+  featMatching(img1, img2, Result_MATCHING);
+  namedWindow("Feature Extraction and Matching");
+  imshow("Feature Extraction and Matching", Result_MATCHING);
+  waitKey(0);
+  return 0;
 }
